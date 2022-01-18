c80262c4ccd006892b12784884393128fdeeb803
kristutis <kristijonas.karalaitis@gmail.com>
2022-01-18 21:46:58 +0200

    sample backend endpoint

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,2 +1,15 @@
-console.log('asdadads ')
-//npm run dev
+import express, {Request, Response} from 'express'
+import { Trainer } from './models/trainer';
+
+const app = express();
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+    const x: Trainer = {
+        moto: "asd",
+        name: 'asdad'
+    }
+    return res.json(x).send()
+})
+
+app.listen(3000, () => console.log('listening on port: 3000')) 
---
+++ back/src/models/trainer.ts
@@ -0,0 +1,5 @@
+import { User } from "./user";
+
+export interface Trainer extends User {
+    moto: string;
+}
