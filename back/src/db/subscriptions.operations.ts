6b785d7662848d51f06bbeae246be444a8c40b23
kristutis <kristijonas.karalaitis@gmail.com>
2022-05-08 22:57:40 +0300

    magic draw changes

--- back/src/db/subscriptions.operations.ts
+++ back/src/db/subscriptions.operations.ts
@@ -2,17 +2,6 @@ import { MysqlError } from 'mysql';
 import { SubscriptionType } from '../models/subscriptionType.model';
 import { db } from './connect';
 
-// function deleteUser(uid: string): Promise<MysqlError | null> {
-// 	return new Promise((resolve, reject) => {
-// 		db.query('DELETE FROM users WHERE uid = ?', [uid], (err, _) => {
-// 			if (err) {
-// 				return reject(err);
-// 			}
-// 			return resolve(null);
-// 		});
-// 	});
-// }
-
 function getSubscriptionTypeByName(
 	name: string
 ): Promise<SubscriptionType | MysqlError> {
